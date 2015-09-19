b4d2290dd22ccf42bc8708330908dc893c3e5e89
maharjun <maharjun@thoughtworks.com>
2015-09-19 14:06:11 +0530

    Game of life should print the output for given input

--- src/com/twu/baselineproblem/GameOfLife.java
+++ src/com/twu/baselineproblem/GameOfLife.java
@@ -1,13 +1,14 @@
 package com.twu.baselineproblem;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class GameOfLife {
 
-    private final UserInput userInput;
-    private final InputValidator inputValidator;
-    private final Delegator delegator;
+    private UserInput userInput;
+    private InputValidator inputValidator;
+    private Delegator delegator;
 
     public GameOfLife(UserInput userInput, InputValidator inputValidator, Delegator delegator) {
         this.userInput = userInput;
@@ -15,17 +16,21 @@ public class GameOfLife {
         this.delegator = delegator;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         UserInput userInput = new UserInput(new BufferedReader(new InputStreamReader(System.in)));
         InputValidator inputValidator = new InputValidator(userInput);
-        Delegator delegator = new Delegator();
-
+        Delegator delegator = new Delegator("");
         GameOfLife gameOfLife = new GameOfLife(userInput, inputValidator, delegator);
-
         gameOfLife.start();
     }
 
-    public void start() {
+    public void start() throws IOException {
+        String input = userInput.getInput();
+        if(inputValidator.validate(input)){
+            delegator = new Delegator(input);
+            System.out.println(delegator.output());
+        }else{
         System.out.println("Invalid Input");
+        }
     }
 }
--- test/com/twu/baselineproblem/GameOfLifeTest.java
+++ test/com/twu/baselineproblem/GameOfLifeTest.java
@@ -8,13 +8,14 @@ import java.io.PrintStream;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.validateMockitoUsage;
 import static org.mockito.Mockito.when;
 
 public class GameOfLifeTest {
 
     @Test
     public void shouldGiveTakeTheInputAndGiveTheOutput() throws IOException {
-        
+
         Delegator delegator = mock(Delegator.class);
         InputValidator inputValidator = mock(InputValidator.class);
         UserInput userInput = mock(UserInput.class);
@@ -26,5 +27,23 @@ public class GameOfLifeTest {
         gameOfLife.start();
 
         assertEquals(outputStream.toString(), "Invalid Input\n");
+    }
+
+    @Test
+    public void shouldGiveTakeTheInputAndprintTheOutput() throws IOException {
+
+        Delegator delegator = mock(Delegator.class);
+        InputValidator inputValidator = mock(InputValidator.class);
+        UserInput userInput = mock(UserInput.class);
+        GameOfLife gameOfLife = new GameOfLife(userInput, inputValidator, delegator);
+        when(userInput.getInput()).thenReturn("----\n-XXX\nXXX-\n----");
+        when(inputValidator.validate("----\n-XXX\nXXX-\n----")).thenReturn(true);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        gameOfLife.start();
+
+
+
+        assertEquals(outputStream.toString(), "--X-\nX--X\nX--X\n-X--\n");
     }
 }
